35666ea83b5108ceacc3bd951e2ea9793b6e3b25
LDdvlp <loicdrouet@gmail.com>
2022-03-19 16:34:55 +0100

    v.1.0.0

---
+++ 003-PHP-Hello_World.php
@@ -0,0 +1,8 @@
+<?php
+        echo "\n"; // new line
+        echo "Hallo World ! (from echo)";
+        echo "\n"; // new line
+        echo "\n"; // new line
+        print "Hallo World ! (from print)";
+        echo "\n"; // new line
+?>
---
+++ README.md
@@ -0,0 +1,6 @@
+# Hello_World_Collection
+Display "Hello World !" in different languages :
+
+1. C
+2. HTML
+3. PHP
